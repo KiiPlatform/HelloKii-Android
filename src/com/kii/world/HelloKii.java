e90f2ba374b91a063ed1830e873182579d7b1967
Moshiur <moshiur.rahman@kii.com>
2014-02-24 17:53:07 +0900

    Fix indentation

--- src/com/kii/world/HelloKii.java
+++ src/com/kii/world/HelloKii.java
@@ -24,13 +24,13 @@ import android.app.Application;
 import com.kii.cloud.storage.Kii;
 
 public class HelloKii extends Application {
-	
+
     @Override
     public void onCreate() {
         super.onCreate();
-                
+
         // initialize the Kii SDK!
         Kii.initialize("__KII_APP_ID__", "__KII_APP_KEY__", __KII_APP_SITE__);
     }
-	
+
 }
